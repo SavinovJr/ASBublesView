1ad6dfbdce93fe9b9f71b4bae124555cc00f9926
Anton Savinov <Anton.Savinov@Harman.com>
2018-03-12 11:24:52 +0300

    Initial Commit

---
+++ ASBublesView/ASBublesView.h
@@ -0,0 +1,19 @@
+//
+//  ASBublesView.h
+//  ASBublesView
+//
+//  Created by Anton Savinov on 12/03/2018.
+//  Copyright © 2018 Harman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASBublesView.
+FOUNDATION_EXPORT double ASBublesViewVersionNumber;
+
+//! Project version string for ASBublesView.
+FOUNDATION_EXPORT const unsigned char ASBublesViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASBublesView/PublicHeader.h>
+
+
